fc0dc3fd1cd5244b3b3600b4fed658daee3e2636
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2017-12-12 12:30:41 +0100

    changes menu titles

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    client_id: process.env.CLIENT_ID || "YOUR_CLIENT_ID",
-    client_secret: process.env.CLIENT_SECRET || "YOUR_CLIENT_SECRET",
+    client_id: process.env.CLIENT_ID || "2549179-0-9c56a3eb-dc1f-11e7-900e-dbb09d4b4347",
+    client_secret: process.env.CLIENT_SECRET || "54498f67-1000-4726-b64e-162604dbedff",
     push_salt: process.env.PUSH_SALT || "Ahshogh3"
 }
