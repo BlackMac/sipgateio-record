602778998954f70c2245389e0437f56743502bc2
Stefan Lange-Hegermann <lange-hegermann@Stefans-Macintosh.local>
2017-12-14 17:31:21 +0100

    XML header

--- controllers/recordhook.js
+++ controllers/recordhook.js
@@ -3,9 +3,9 @@ const config = require("../config")
 const md5 = require("js-md5")
 
 exports.post = (req, res) => {
-  console.log(req)
     let callId = req.body.callId
     let hookEndpoint = req.protocol + '://' + req.get('host') + "/record/" + md5(config.push_salt+req.session.masterSipId)
+    res.append('Content-Type', 'application/xml');
     if (req.body.event == "answer") {
         sipgate.startRecording(req.session.bearer, callId).catch(console.warn)
         return res.send('<?xml version="1.0" encoding="UTF-8"?><Response />')
