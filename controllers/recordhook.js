35340975757d7fff61fbd9f3087a9d898e48c2e8
Stefan Lange-Hegermann <lange-hegermann@Stefans-Macintosh.local>
2017-12-14 14:24:02 +0100

    recording

--- controllers/recordhook.js
+++ controllers/recordhook.js
@@ -1,7 +1,10 @@
 const sipgate = require("../models/sipgate")
+const config = require("../config")
+const md5 = require("js-md5")
 
 exports.post = (req, res) => {
     let callId = req.body.callId
+    let hookEndpoint = req.protocol + '://' + req.get('host') + "/record/" + md5(config.push_salt+req.session.masterSipId)
     sipgate.startRecording(req.session.bearer, callId).catch(console.warn)
-    res.send('<?xml version="1.0" encoding="UTF-8"?><Response></Response>')
+    res.send('<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="'+hookEndpoint+'" />')
 }
