280dd2e96f774a32cdffec9fd2f789b33e5af757
Stefan Lange-Hegermann <lange-hegermann@Stefans-Macintosh.local>
2017-12-14 17:13:51 +0100

    adds logging

--- controllers/recordhook.js
+++ controllers/recordhook.js
@@ -3,8 +3,12 @@ const config = require("../config")
 const md5 = require("js-md5")
 
 exports.post = (req, res) => {
+  console.log(req)
     let callId = req.body.callId
     let hookEndpoint = req.protocol + '://' + req.get('host') + "/record/" + md5(config.push_salt+req.session.masterSipId)
-    sipgate.startRecording(req.session.bearer, callId).catch(console.warn)
-    res.send('<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="'+hookEndpoint+'" />')
+    if (req.body.event == "answer") {
+        sipgate.startRecording(req.session.bearer, callId).catch(console.warn)
+        return res.send('<?xml version="1.0" encoding="UTF-8"?><Response />')
+    }
+    return res.send('<?xml version="1.0" encoding="UTF-8"?><Response onAnswer="'+hookEndpoint+'" />')
 }
